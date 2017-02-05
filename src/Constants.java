38f19331489c2403901e5485cdbe89574efb6d34
Mursith <nmursith@gmail.com>
2017-02-05 21:35:58 +0530

    change

--- src/Constants.java
+++ src/Constants.java
@@ -22,4 +22,6 @@ public class Constants {
     public static String JOIN = "JOIN";
     public static String LEAVE = "LEAVE";
 
+    public static String BOOTSTRAP_SERVER_IP = ReadFile.getBootStrapSever();//IPLIKE + ".8.102";
+
 }
